b7661454b7b1cc6ebe80c8f5c2ed4cad14e1442d
Wesley Hu <whu@mines.edu>
2024-09-27 03:06:13 -0600

    remove

--- whu-portfolio/src/components/Encrypt.jsx
+++ whu-portfolio/src/components/Encrypt.jsx
@@ -11,20 +11,20 @@ const Encrypt = () => {
 
 const CYCLES_PER_LETTER = 2;
 const SHUFFLE_TIME = 50;
-const CYCLE_INTERVAL = 2000; // 2 seconds between scrambles
+const CYCLE_INTERVAL = 2000;
 const CHARS = "!@#$%^&*():{};|,.<>/?";
 
 const EncryptText = ({ texts }) => {
   const scrambleIntervalRef = useRef(null);
   const cycleIntervalRef = useRef(null);
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
-  const [text, setText] = useState(texts[0]); // Start with the first text
+  const [text, setText] = useState(texts[0]);
 
   const scramble = () => {
     let pos = 0;
     scrambleIntervalRef.current = setInterval(() => {
       const scrambled = texts[currentTextIndex]
-        .split("") // Use the current text for scrambling
+        .split("")
         .map((char, index) => {
           if (pos / CYCLES_PER_LETTER > index) {
             return char;
@@ -48,25 +48,22 @@ const EncryptText = ({ texts }) => {
 
   const stopScramble = () => {
     clearInterval(scrambleIntervalRef.current || undefined);
-
-    // Immediately switch to the next text after scramble ends
-    setText(texts[currentTextIndex]); // Reset to the current text
+    setText(texts[currentTextIndex]);
     setCurrentTextIndex((prevIndex) =>
       prevIndex === texts.length - 1 ? 0 : prevIndex + 1
     );
   };
 
   useEffect(() => {
-    // Start the cycle of scrambling every CYCLE_INTERVAL
     cycleIntervalRef.current = setInterval(() => {
-      scramble(); // Scramble the current text at the specified interval
+      scramble();
     }, CYCLE_INTERVAL);
 
     return () => {
-      clearInterval(cycleIntervalRef.current); // Clear interval on component unmount
-      clearInterval(scrambleIntervalRef.current); // Clear scramble interval
+      clearInterval(cycleIntervalRef.current);
+      clearInterval(scrambleIntervalRef.current);
     };
-  }, [currentTextIndex]); // Re-run effect when text index changes
+  }, [currentTextIndex]);
 
   return (
     <motion.div className="font-rubik text-blood-red text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-extrabold text-nowrap select-none">
