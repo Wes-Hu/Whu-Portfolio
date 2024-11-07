e0e69d384dd0876410734fc74d4355771f153b84
Wesley Hu <whu@mines.edu>
2024-11-07 16:17:11 -0700

    updated skills section

--- whu-portfolio/src/components/MarqueeChild.jsx
+++ whu-portfolio/src/components/MarqueeChild.jsx
@@ -3,10 +3,10 @@ import { motion } from "framer-motion";
 const MarqueeChild = ({children}) => {
     return (
         <motion.div
-            initial={{ scale: 1 }}
-            whileHover={{ scale: 1.2 }}
+            initial={{ scale: 1, color: "#090A0C", rotate: 0}}
+            whileHover={{ scale: 1.2, color: "#EE6C4D", rotate: "3deg" }}
             transition={{ duration: 0.1, ease: 'easeInOut'}} 
-            className="inline-flex m-2 bg-blood-red px-3 py-2 mx-3 my-6 italic rounded-xl font-semibold z-30 font-raleway transition-all duration-300 ease-in-out hover:text-burnt-sienna select-none"
+            className="inline-flex m-2 bg-blood-red px-3 py-2 mx-3 my-6 italic rounded-xl font-semibold z-30 font-raleway select-none"
         >
             {children}
         </motion.div>
