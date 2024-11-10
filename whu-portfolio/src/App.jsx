3fc2473d591924623d9d8db2fa8c56035b7c39ff
Wesley Hu <whu@mines.edu>
2024-11-10 16:34:34 -0700

    header messeed up

--- whu-portfolio/src/App.jsx
+++ whu-portfolio/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="flex flex-col z-10 cursor-none">
       <Cursor/>
-      <header className="fixed top-0 left-0 w-screen h-24 z-50 flex flex-row justify-between px-3 md:px-10 items-center cursor-auto overflow-hidden">
+      <header className="fixed top-0 left-0 w-screen h-24 z-50 flex flex-row justify-between px-3 md:px-10 items-center cursor-auto">
         <HomeButton/>
         <NavMenu/>
       </header>
