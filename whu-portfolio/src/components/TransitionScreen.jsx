00aeab0c889dbcea8be9c652648cebe44e9e179a
Wesley Hu <whu@mines.edu>
2025-04-02 15:34:11 -0600

    changed transition screen to match loading screen

--- whu-portfolio/src/components/TransitionScreen.jsx
+++ whu-portfolio/src/components/TransitionScreen.jsx
@@ -3,20 +3,27 @@ import { motion } from "framer-motion";
 const TransitionScreen = ({text}) => {
     return (
         <motion.div
-            initial={{ scale: 0, x: "-50%", y: "-50%" }}
-            animate={{ scale: 1, x: "-50%", y: "-50%", transition: { duration: 1, ease: "easeInOut" } }}
-            exit={{ scale: 0, x: "-50%", y: "-50%", transition: { duration: 1, ease: "easeInOut", delay: 0.5 } }}
-            className="fixed top-1/2 left-1/2 w-[300vw] h-[300vw]  rounded-full bg-blood-red z-50 flex items-center justify-center"
+            initial={{ y: "-100%" }}
+            animate={{  y: 0, transition: { duration: 1, ease: "easeInOut" } }}
+            exit={{ y: "-100%", transition: { duration: 1, ease: "easeInOut", delay: 0.75 } }}
+            className="fixed top-0 left-0 z-50 flex flex-col"
           >
-            <motion.h1
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1, transition: { duration: 0.5, ease: "easeInOut", delay: 0.5 } }}
-                exit={{ opacity: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
+            <motion.div className="w-screen h-screen bg-blood-red flex items-center justify-center">
+                <motion.h1
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1, transition: { duration: 0.5, ease: "easeInOut", delay: 0.75 } }}
+                    exit={{ opacity: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
 
-                className="font-rubik font-extrabold text-night text-5xl"
-            >
-                {text}        
-            </motion.h1>
+                    className="font-rubik font-extrabold text-night text-5xl"
+                >
+                    {text}        
+                </motion.h1>
+            </motion.div>
+            <img
+                src="/loading.svg"
+                alt="Loading Screen"
+                className="w-screen h-screen object-cover z-40 -translate-y-10"
+            />            
           </motion.div>
     );
 }
